bf6c2e0ae76159568f16039b0c6dcca4142377eb
Guilherme Moraes <guimoraes.dev@gmail.com>
2024-04-01 10:46:23 -0300

    feat: initial setup

---
+++ apps/admin/tailwind.config.ts
@@ -0,0 +1,19 @@
+import { createGlobPatternsForDependencies } from '@nx/react/tailwind';
+import { join } from 'path';
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  presets: [require('../../tailwind.preset')],
+  content: [
+    join(
+      __dirname,
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+export default config;
